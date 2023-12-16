f6460c268dd6b8befa05fd78ac2ffd8c336d12a7
kimhm0728 <kimhm0728@naver.com>
2023-12-16 15:19:40 +0900

    feat: 평일, 휴일, 법정 공휴일을 구분하는 기능 구현

---
+++ src/main/kotlin/oncall/service/DayClassifier.kt
@@ -0,0 +1,38 @@
+package oncall.service
+
+import oncall.constants.DayOfWeek
+import oncall.constants.Month
+
+object DayClassifier {
+
+    private val legalHoliDayStore = mutableMapOf<Month, Int>()
+
+    init {
+        legalHoliDayStore[Month.JAN] = 1
+        legalHoliDayStore[Month.MAR] = 1
+        legalHoliDayStore[Month.MAY] = 5
+        legalHoliDayStore[Month.JUN] = 6
+        legalHoliDayStore[Month.AUG] = 15
+        legalHoliDayStore[Month.OCT] = 3
+        legalHoliDayStore[Month.OCT] = 9
+        legalHoliDayStore[Month.DEC] = 25
+    }
+
+    fun isWeekDay(dayOfWeek: DayOfWeek): Boolean {
+        if (dayOfWeek == DayOfWeek.MON) return true
+        if (dayOfWeek == DayOfWeek.TUE) return true
+        if (dayOfWeek == DayOfWeek.WED) return true
+        if (dayOfWeek == DayOfWeek.THU) return true
+        if (dayOfWeek == DayOfWeek.FRI) return true
+        return false
+    }
+
+    fun isHoliDay(dayOfWeek: DayOfWeek) =
+        !isWeekDay(dayOfWeek)
+
+    fun isLegalHoliDay(month: Month, day: Int): Boolean {
+        val legalDay = legalHoliDayStore[month] ?: return false
+        if (legalDay == day) return true
+        return false
+    }
+}
