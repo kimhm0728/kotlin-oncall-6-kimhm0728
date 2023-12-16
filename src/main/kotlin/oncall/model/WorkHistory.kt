f529e5226ffb149b2f6adf53d9d2d65b46942174
kimhm0728 <kimhm0728@naver.com>
2023-12-16 16:41:28 +0900

    feat: 근무자별 근무 날짜를 저장하는 기능 구현

---
+++ src/main/kotlin/oncall/model/WorkHistory.kt
@@ -0,0 +1,18 @@
+package oncall.model
+
+object WorkHistory {
+
+    private val history = mutableMapOf<String, MutableList<Int>>()
+
+    fun work(worker: Worker, day: Int) {
+        if (history.contains(worker.toString())) {
+            history[worker.toString()]?.add(day)
+            return
+        }
+
+        history[worker.toString()] = mutableListOf(day)
+    }
+
+    fun isWorkYesterday(worker: Worker, day: Int) =
+        history[worker.toString()]?.contains(day - 1) == true
+}
