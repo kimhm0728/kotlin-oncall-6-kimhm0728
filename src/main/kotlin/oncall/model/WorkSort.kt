015cecead6020e587a9223593766f2a502ebbe9a
kimhm0728 <kimhm0728@naver.com>
2023-12-16 14:22:44 +0900

    feat: 근무자가 평일, 휴일 각각 1번씩 편성되어있는지 체크하는 기능 구현

--- src/main/kotlin/oncall/model/WorkSort.kt
+++ src/main/kotlin/oncall/model/WorkSort.kt
@@ -1,3 +1,15 @@
 package oncall.model
 
-class WorkSort(val workers: List<Worker>)
+class WorkSort(private val workers: List<Worker>) {
+
+    fun validateWorkOnce(otherWorkSort: WorkSort) {
+        validateSize(otherWorkSort)
+        validateContainsAllWorker(otherWorkSort)
+    }
+
+    private fun validateSize(otherWorkSort: WorkSort) =
+        require(workers.size == otherWorkSort.workers.size)
+
+    private fun validateContainsAllWorker(otherWorkSort: WorkSort) =
+        require(workers.all { otherWorkSort.workers.contains(it) })
+}
