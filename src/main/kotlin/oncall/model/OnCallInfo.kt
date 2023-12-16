675510d1be08a606619b5bc91a0b50244b81d8fb
kimhm0728 <kimhm0728@naver.com>
2023-12-16 15:03:37 +0900

    feat: 월과 시작 요일에 맞게 날짜를 계산하는 기능 구현

--- src/main/kotlin/oncall/model/OnCallInfo.kt
+++ src/main/kotlin/oncall/model/OnCallInfo.kt
@@ -4,4 +4,39 @@ import oncall.constants.Day
 import oncall.constants.Month
 
 
-class OnCallInfo(val month: Month, val startDay: Day)
+class OnCallInfo(private val month: Month, private val startDay: Day) {
+
+    private val dayStore = mutableMapOf<Int, Day>()
+
+    init {
+        initDayStore()
+    }
+
+    private fun initDayStore() {
+        enumValues<Day>().forEach { day ->
+            if (day.ordinal >= startDay.ordinal) {
+                dayStore[day.ordinal- startDay.ordinal] = day
+            }
+        }
+
+        if (dayStore.size == WEEK) return
+
+        enumValues<Day>().forEach { day ->
+            if (day.ordinal < startDay.ordinal) {
+                dayStore[WEEK - startDay.ordinal + day.ordinal] = day
+            }
+        }
+    }
+
+    fun forEach(action: (Int, Day) -> Unit) {
+        for (day in 1..month.day) {
+            action(day, getDay(day))
+        }
+    }
+
+    private fun getDay(day: Int) = dayStore[(day -  1) % WEEK]!!
+
+    companion object {
+        private const val WEEK = 7
+    }
+}
