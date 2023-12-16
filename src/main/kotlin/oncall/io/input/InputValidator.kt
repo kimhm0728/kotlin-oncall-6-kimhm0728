3ab66bc84a202676771293a2d68702b0780c0f63
kimhm0728 <kimhm0728@naver.com>
2023-12-16 14:11:29 +0900

    feat: 비상 근무 순서 입력 예외 처리 기능 구현

--- src/main/kotlin/oncall/io/input/InputValidator.kt
+++ src/main/kotlin/oncall/io/input/InputValidator.kt
@@ -22,4 +22,27 @@ class InputValidator {
     private fun String.validateMonth() = require(Month.entries.any { it.title == this })
 
     private fun String.validateStartDay() = require(Day.entries.any { it.title == this })
+
+    fun validateWorkSort(input: String) {
+        input.validateEmpty()
+        with(input.convertListWithComma()) {
+            validateWorkerSize()
+            validateWorkerDuplication()
+            validateWorkerNickName()
+        }
+    }
+
+    private fun List<String>.validateWorkerSize() =
+        require(size in WORKER_RANGE)
+
+    private fun List<String>.validateWorkerDuplication() =
+        require(distinct().size == size)
+
+    private fun List<String>.validateWorkerNickName() =
+        require(all { NICKNAME_REGEX.matches(it) })
+
+    companion object {
+        private val WORKER_RANGE = 5..35
+        private val NICKNAME_REGEX = "^[ㄱ-ㅎ가-힣]{1,5}$".toRegex()
+    }
 }
