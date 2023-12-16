739cc9a7dd9e4c17cc6747d5f74784917bfb7616
kimhm0728 <kimhm0728@naver.com>
2023-12-16 16:02:02 +0900

    feat: 근무자 클래스 Worker의 nickname이 같으면 같은 객체로 판단하는 기능 구현

--- src/main/kotlin/oncall/model/Worker.kt
+++ src/main/kotlin/oncall/model/Worker.kt
@@ -1,5 +1,11 @@
 package oncall.model
 
 class Worker(private val nickname: String) {
+
+    override fun hashCode() = nickname.hashCode()
+
+    override fun equals(other: Any?) =
+        this.nickname == (other as Worker).nickname
+
     override fun toString() = nickname
 }
