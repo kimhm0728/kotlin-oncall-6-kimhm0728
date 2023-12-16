e1bc7816e57ae3605a0abc9d5bd184095a37a9a0
kimhm0728 <kimhm0728@naver.com>
2023-12-16 17:08:29 +0900

    style: 월마다 며칠로 구성되어 있는지를 나타내는 enum값 수정

--- src/main/kotlin/oncall/constants/Month.kt
+++ src/main/kotlin/oncall/constants/Month.kt
@@ -4,13 +4,13 @@ enum class Month(val title: String, val day: Int) {
     JAN("1", 31),
     FEB("2", 28),
     MAR("3", 31),
-    APR("4", 31),
+    APR("4", 30),
     MAY("5", 31),
-    JUN("6", 31),
+    JUN("6", 30),
     JUL("7", 31),
     AUG("8", 31),
-    SEP("9", 31),
+    SEP("9", 30),
     OCT("10", 31),
-    NOV("11", 31),
+    NOV("11", 30),
     DEC("12", 31);
 }
